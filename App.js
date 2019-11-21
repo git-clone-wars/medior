5b71dcc02f96cb7fd37c55f227c8b73a4745eff2
Greta Gadbois <ggadbois@hmc.edu>
2019-11-21 17:53:08 -0500

    start of scanner component

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {AppRegistry, StyleSheet, Text, View } from 'react-native';
+import {Scanner} from './client/components'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Scanner />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +17,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
