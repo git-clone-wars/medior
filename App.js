cb3136e4a5be7ca70afd0af0235e685f762a7f2e
alewoo28 <alewoo28@gmail.com>
2019-11-22 11:58:00 -0500

    firebase config class made

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FirebaseWrapper } from './server/db/firebase';
+import { firebaseConfig } from './server/db/config'
 
 export default function App() {
+
+  FirebaseWrapper.GetInstance().Initialize(firebaseConfig)
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
